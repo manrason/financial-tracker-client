2547b0bdc13cee81bb0e51f83c823233ea9b03c3
manrason <manrason2@gmail.com>
2024-01-11 23:39:58 +0100

    rest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { FormsModule } from '@angular/forms';
--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Expense } from './expense';
+import { ExpenseService } from '../expense.service';
 
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
   styleUrls: ['./expenses.component.css']
 })
-export class ExpensesComponent {
-  expense: Expense ={
-    expenseId: 1,
-    name: "titre",
-    amount: 10.25,
-    creationDate: new Date(),
-    comments: "commentaire"
-  };
+export class ExpensesComponent implements OnInit {
+  
+
+  expenses: Expense[] = [];
+  constructor (private expenseService: ExpenseService) {}
+
+  ngOnInit(): void {
+    this.getExpenses;
+  }
+
+  getExpenses(): void {
+    this.expenseService.getExpenses().subscribe(expenses => this.expenses = expenses);
+  }
+  
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.expenseService.addExpense({ name } as Expense)
+      .subscribe(expense => {
+        this.expenses.push(expense);
+      });
+  }
+
+  delete(expense: Expense): void {
+    this.expenses = this.expenses.filter(h => h !== expense);
+    this.expenseService.deleteExpense(expense.expenseId).subscribe();
+  }
 }
